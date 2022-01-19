4b8d1d88cca2925ee873cb4f56224aade6baae3d
Jamie <jamie-cartwright@digitalcareerinstitute.org>
2022-01-19 09:50:39 +0100

    Update content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bonjour tout le monde
+          Hallo Welt
         </p>
         <a
           className="App-link"
